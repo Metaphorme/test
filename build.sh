2d208d5321df60c0cc1112fcb7d4ce4c952264e0
Diazepam <102651183+Metaphorme@users.noreply.github.com>
2022-11-30 12:45:33 +0800

    Update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Start to build for docker..."
 
 /usr/bin/python3 -m http.server 28294 &
-curl -O http://127.0.0.1:28294/LICENSE
+curl http://127.0.0.1:28294
 
 #docker build -t rosetta - < Dockerfile
 #kill -9 $(netstat -antp | grep :28294 | awk '{print $7}' | awk -F'/' '{ print $1 }')
